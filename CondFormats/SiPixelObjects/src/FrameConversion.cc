cb67cfb09d90fa814b6dbc155f6de88f31fdfd5c
Tamas Vami <vami.tamas@wigner.mta.hu>
2017-07-03 23:52:52 +0200

    Update FrameConversion.cc

--- CondFormats/SiPixelObjects/src/FrameConversion.cc
+++ CondFormats/SiPixelObjects/src/FrameConversion.cc
@@ -2,169 +2,13 @@
 #include "DataFormats/SiPixelDetId/interface/PixelBarrelName.h"
 #include "DataFormats/SiPixelDetId/interface/PixelEndcapName.h"
 #include "CondFormats/SiPixelObjects/interface/LocalPixel.h"
+
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 using namespace std;
 using namespace edm;
 using namespace sipixelobjects;
 
-FrameConversion::FrameConversion(bool bpix, int side, int layer, int rocIdInDetUnit) {
-  int slopeRow =0;
-  int slopeCol = 0;
-  int  rowOffset = 0;
-  int  colOffset = 0; 
- 
-  if (bpix ) { // bpix 
-    
-    if (side==-1 && layer!=1) {  // -Z side: 4 non-flipped modules oriented like 'dddd', except Layer 1
-
-      if (rocIdInDetUnit <8) {
-	slopeRow = 1;
-	slopeCol = -1;
-	rowOffset = 0;
-	colOffset = (8-rocIdInDetUnit)*LocalPixel::numColsInRoc-1;	
-      } else {
-	slopeRow = -1;
-	slopeCol = 1;      
-	rowOffset = 2*LocalPixel::numRowsInRoc-1;
-	colOffset = (rocIdInDetUnit-8)*LocalPixel::numColsInRoc;
-      } // if roc
-      
-    } else {  // +Z side: 4 non-flipped modules oriented like 'pppp', but all 8 in Layer 1
-      
-      if (rocIdInDetUnit <8) {
-	slopeRow = -1;
-	slopeCol = 1;
-	rowOffset = 2*LocalPixel::numRowsInRoc-1;
-	colOffset = rocIdInDetUnit * LocalPixel::numColsInRoc; 
-      } else {
-	slopeRow = 1;
-	slopeCol = -1;
-	rowOffset = 0;
-	colOffset = (16-rocIdInDetUnit)*LocalPixel::numColsInRoc-1; 
-      }
-      
-    } // end if +-Z
-
-
-
-  } else { // fpix 
-
-    // for fpix follow Urs's code for pilot blade
-    // no difference between panels
-    if(side==-1) { // pannel 1
-      if (rocIdInDetUnit < 8) {
-	slopeRow = 1;
-	slopeCol = -1;
-	rowOffset = 0;
-	colOffset = (8-rocIdInDetUnit)*LocalPixel::numColsInRoc-1;
-      } else {
-	slopeRow = -1;
-	slopeCol = 1;
-	rowOffset = 2*LocalPixel::numRowsInRoc-1;
-	colOffset = (rocIdInDetUnit-8)*LocalPixel::numColsInRoc;
-      }
-    } else { // pannel 2 
-      if (rocIdInDetUnit < 8) {
-	slopeRow = 1;
-	slopeCol = -1;
-	rowOffset = 0;
-	colOffset = (8-rocIdInDetUnit)*LocalPixel::numColsInRoc-1;
-      } else {
-	slopeRow = -1;
-	slopeCol = 1;
-	rowOffset = 2*LocalPixel::numRowsInRoc-1;
-	colOffset = (rocIdInDetUnit-8)*LocalPixel::numColsInRoc;
-      }
-      
-    } // side 
-
-  } // bpix/fpix
-
-  theRowConversion      = LinearConversion(rowOffset,slopeRow);
-  theCollumnConversion =  LinearConversion(colOffset, slopeCol);
-  
-}
-
-FrameConversion::FrameConversion(bool bpix, int side, int rocIdInDetUnit) {
-  int slopeRow =0;
-  int slopeCol = 0;
-  int  rowOffset = 0;
-  int  colOffset = 0; 
- 
-  if (bpix ) { // bpix 
-    
-    if (side==-1) {  // -Z side
-
-      if (rocIdInDetUnit <8) {
-	slopeRow = 1;
-	slopeCol = -1;
-	rowOffset = 0;
-	colOffset = (8-rocIdInDetUnit)*LocalPixel::numColsInRoc-1;	
-      } else {
-	slopeRow = -1;
-	slopeCol = 1;      
-	rowOffset = 2*LocalPixel::numRowsInRoc-1;
-	colOffset = (rocIdInDetUnit-8)*LocalPixel::numColsInRoc;
-      } // if roc
-      
-    } else {  // +Z side
-      
-      if (rocIdInDetUnit <8) {
-	slopeRow = -1;
-	slopeCol = 1;
-	rowOffset = 2*LocalPixel::numRowsInRoc-1;
-	colOffset = rocIdInDetUnit * LocalPixel::numColsInRoc; 
-      } else {
-	slopeRow = 1;
-	slopeCol = -1;
-	rowOffset = 0;
-	colOffset = (16-rocIdInDetUnit)*LocalPixel::numColsInRoc-1; 
-      }
-      
-    } // end if +-Z
-
-
-
-  } else { // fpix 
-
-    // for fpix follow Urs's code for pilot blade
-    // no difference between panels
-    if(side==-1) { // pannel 1
-      if (rocIdInDetUnit < 8) {
-	slopeRow = 1;
-	slopeCol = -1;
-	rowOffset = 0;
-	colOffset = (8-rocIdInDetUnit)*LocalPixel::numColsInRoc-1;
-      } else {
-	slopeRow = -1;
-	slopeCol = 1;
-	rowOffset = 2*LocalPixel::numRowsInRoc-1;
-	colOffset = (rocIdInDetUnit-8)*LocalPixel::numColsInRoc;
-      }
-    } else { // pannel 2 
-      if (rocIdInDetUnit < 8) {
-	slopeRow = 1;
-	slopeCol = -1;
-	rowOffset = 0;
-	colOffset = (8-rocIdInDetUnit)*LocalPixel::numColsInRoc-1;
-      } else {
-	slopeRow = -1;
-	slopeCol = 1;
-	rowOffset = 2*LocalPixel::numRowsInRoc-1;
-	colOffset = (rocIdInDetUnit-8)*LocalPixel::numColsInRoc;
-      }
-      
-    } // side 
-
-  } // bpix/fpix
-
-  theRowConversion      = LinearConversion(rowOffset,slopeRow);
-  theCollumnConversion =  LinearConversion(colOffset, slopeCol);
-  
-}
-
-// OLD method for phase0 bpix
 FrameConversion::FrameConversion( const PixelBarrelName & name, int rocIdInDetUnit)
 {
   int slopeRow =0;
@@ -231,7 +75,6 @@ FrameConversion::FrameConversion( const PixelBarrelName & name, int rocIdInDetUn
   theCollumnConversion =  LinearConversion(colOffset, slopeCol);
 
 }
-// OLD method for phase0 fpix
 FrameConversion::FrameConversion( const PixelEndcapName & name, int rocIdInDetUnit)
 {
   int slopeRow =0;
@@ -312,6 +155,22 @@ FrameConversion::FrameConversion( const PixelEndcapName & name, int rocIdInDetUn
         colOffset = (rocIdInDetUnit-5)*LocalPixel::numColsInRoc;
         rowOffset = 2*LocalPixel::numRowsInRoc-1;
       } 
+    }
+  }
+
+  if (name.diskName()==3) {
+    if (rocIdInDetUnit <8) {
+      slopeRow = 1;
+      slopeCol = -1;  
+
+      rowOffset = 0;
+      colOffset = (8-rocIdInDetUnit)*LocalPixel::numColsInRoc-1;
+    } else {
+      slopeRow = -1;
+      slopeCol = 1;
+   
+      rowOffset = 2*LocalPixel::numRowsInRoc-1;
+      colOffset = (rocIdInDetUnit-8)*LocalPixel::numColsInRoc;
     }
   }
 
