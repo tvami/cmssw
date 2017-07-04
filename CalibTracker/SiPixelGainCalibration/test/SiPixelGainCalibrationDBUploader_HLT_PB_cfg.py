f96f8f0423b94db4ab6255b3c69868caaa40bf9d
Tamas Vami <vami.tamas@wigner.mta.hu>
2017-07-04 12:05:42 +0200

    Add files via upload

---
+++ CalibTracker/SiPixelGainCalibration/test/SiPixelGainCalibrationDBUploader_HLT_PB_cfg.py
@@ -0,0 +1,83 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+process = cms.Process("PixelGainDB",eras.Run2_25ns)
+# Services 
+process.load('Configuration.StandardSequences.Services_cff')
+process.load("Configuration.StandardSequences.GeometrySimDB_cff")
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+
+# GlobalTag
+# --------------------- GlobalTag ----------------------
+from Configuration.AlCa.GlobalTag import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, '80X_dataRun2_Prompt_v11', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
+
+
+from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import*
+import CalibTracker.Configuration.Common.PoolDBESSource_cfi
+
+# ---------------------- PB Geometry -------------------
+process.trackerGeometryDB.applyAlignment = cms.bool(False)
+# should it be False?
+process.XMLFromDBSource.label=''
+process.PoolDBESSourceGeometry = cms.ESSource("PoolDBESSource",
+  timetype = cms.string('runnumber'),
+  connect = cms.string('frontier://FrontierPrep/CMS_CONDITIONS'),
+  toGet = cms.VPSet(
+    cms.PSet(
+      record = cms.string('GeometryFileRcd'),
+      tag = cms.string('SiPixelPilotGeometry_v1')
+    ),
+    cms.PSet(
+      record = cms.string('IdealGeometryRecord'),
+      tag = cms.string('TKRECO_Geometry_forPilotBlade_v1')
+    ),
+    cms.PSet(
+      record = cms.string('PGeometricDetExtraRcd'),
+      tag = cms.string('TKExtra_Geometry_forPilotBlade_v1')
+    ),
+    cms.PSet(
+      record = cms.string('PTrackerParametersRcd'),
+      tag = cms.string('TKParameters_Geometry_forPilotBlade_v1')
+    ),
+  )
+)
+process.es_prefer_geometry = cms.ESPrefer( "PoolDBESSource", "PoolDBESSourceGeometry" )
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
+
+process.gainDBHLT = cms.EDAnalyzer("SiPixelGainCalibrationDBUploader",
+    inputrootfile = cms.untracked.string('/data/vami/projects/pilotBlade/GainDB/GainCalibration.root'),
+    record = cms.untracked.string('SiPixelGainCalibrationForHLTRcd'),
+    useMeanWhenEmpty = cms.untracked.bool(True),
+    badChi2Prob = cms.untracked.double(0.00001)
+    )
+
+process.source = cms.Source("EmptyIOVSource",
+    #lastRun = cms.untracked.uint32(1),
+    timetype = cms.string('runnumber'),
+    firstValue = cms.uint64(1),
+    lastValue = cms.uint64(1),
+    interval = cms.uint64(1)
+    )
+
+
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+    BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+    DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(10),
+        authenticationPath = cms.untracked.string('.')
+        ),
+    toPut = cms.VPSet(
+        cms.PSet(
+            record = cms.string('SiPixelGainCalibrationForHLTRcd'),
+            tag = cms.string('GainCalib_PilotBlade_hlt_data_v1')
+            )
+        ),
+    connect = cms.string('sqlite_file:./GainDB_HLT.db')
+)
+
+process.p = cms.Path(process.gainDBHLT)
+
+
